5519ffdfc33d9db92492d141c8b2bb946b5354fc
Utilisateur <Utilisateur@Utilisateur-PC>
2014-05-26 10:01:34 +0200

    Prepare server Signed-off-by:Utilisateur <Utilisateur@Utilisateur-PC>

--- Server Side/app.js
+++ Server Side/app.js
@@ -1,8 +1,24 @@
-var clients = {};
+var everyones = {};
+
+
+
+function Link(screen){
+    this.screen = screen;
+    this.device = false;
+    
+    this.connectNewDevice = function(connected){
+        this.device = connected;
+    };
+}
+
+function Connexion(socket, type){
+    this.socket = socket;
+    this.type = type;
+}
 
 function getId(){
-    var id = Math.random().toString(36).substr(2, 9);
-    if(!clients[id])
+    var id = Math.random().toString(36).substr(2, 20);
+    if(!everyones[id])
         return id;
     else return getId();
 }
